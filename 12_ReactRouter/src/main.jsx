fa134370aed4b5f6bdea5c36d77e5a09230d6df3
Md Mustaqim Alam <mustaqimalam85@gamil.com>
2024-01-08 08:00:22 +0530

    12Lacture

---
+++ 12_ReactRouter/src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./Root.jsx";
+import Home from "./Components/Home/Home.jsx";
+import About from "./Components/About/about.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "",
+       element: <Home /> },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
