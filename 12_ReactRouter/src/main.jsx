1ec3a7c5f80451575b9bd8284f2c8b68f994aafa
Md Mustaqim Alam <mustaqimalam85@gamil.com>
2024-01-08 08:15:01 +0530

    Your commit message here

--- 12_ReactRouter/src/main.jsx
+++ 12_ReactRouter/src/main.jsx
@@ -4,22 +4,44 @@ import Root from "./Root.jsx";
 import Home from "./Components/Home/Home.jsx";
 import About from "./Components/About/about.jsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Contact from "./Components/Contact/Contact.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      { path: "",
-       element: <Home /> },
-      {
-        path: "about",
-        element: <About />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+
+//   {
+//     path: "/",
+//     element: <Root />,
+//     children: [
+//       { path: "",
+//        element: <Home /> },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       }
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="" element={<Home />} />,
+      <Route path="about" element={<About />} />,
+      <Route path="contact" element={<Contact />} />,
+    </Route>
+    
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
